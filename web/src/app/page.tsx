0d51a1d473dc95725db9fac9f85972a2d9791922
Dominik Kundel <hi@dominik.dev>
2024-07-14 14:10:46 -0700

    add web interface from hackathon

---
+++ web/src/app/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Landing } from "@/components/landing";
+import { Podcast, SAMPLE_MESSAGES } from "@/components/podcast";
+import Image from "next/image";
+import { useState } from "react";
+import { getPodcast } from "./actions";
+
+export default function Home() {
+  const [data, setData] = useState<{ topic: string; speakers: string[] }>();
+  const [podcastContent, setPodcast] = useState<any>(null);
+  const [useOpenai, setOpenai] = useState(true);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between  p-24 text-white bg-gradient-to-b bg-slate-900 from-slate-900 to-purple-900/30">
+      <label className="text-xs fixed top-2 right-4 accent-purple-500 text-slate-300 ">
+        Use OpenAI directly{" "}
+        <input
+          className="align-center"
+          checked={useOpenai}
+          type="checkbox"
+          onChange={() => setOpenai((current) => !current)}
+        />
+      </label>
+      {!data && (
+        <Landing
+          onStart={async (data) => {
+            const podcastUi = await getPodcast(
+              data.topic,
+              data.speakers,
+              useOpenai
+            );
+            setPodcast(podcastUi);
+            setData(data);
+          }}
+        />
+      )}
+      {data && (
+        <Podcast topic={data?.topic} speakers={data?.speakers || []}>
+          {podcastContent}
+        </Podcast>
+      )}
+    </main>
+  );
+}
