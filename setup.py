24a69989464386719556e00442334ec30468aaf2
George <zkxqxkz2@gmail.com>
2024-07-14 23:34:07 -0400

    server

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 from setuptools import setup, find_packages
 
+
+extra_reqs = {
+    "dev": [
+        "pytest==8.2.2",
+        "black==24.4.2",
+        "ruff==0.5.0",
+    ],
+}
+
 setup(
-    name='podcast_gpt',
-    version='0.1',
-    description='A Python package for processing podcasts using GPT',
-    author='Matthew',
-    author_email='mmym.ezout@gmail.com',
-    url='https://github.com/alex/podcast_agent_gpt',
+    name="podcast_gpt",
+    version="0.1",
+    description="A Python package for processing podcasts using GPT",
+    author="Matthew",
+    author_email="mmym.ezout@gmail.com",
+    url="https://github.com/alex/podcast_agent_gpt",
     packages=find_packages(),
     install_requires=[
-        'elevenlabs',
-        'tqdm',
-        'mutagen',
-        'langchain',
+        "elevenlabs",
+        "tqdm",
+        "mutagen",
+        "langchain",
+        "fastapi",
     ],
+    extras_require=extra_reqs,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.11',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
     ],
 )
