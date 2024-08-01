2d79525b4775275ad6f38278b5e00a32e24bab41
supermomo668 <mymomo119966.mm@gmail.com>
2024-08-01 23:26:02 +0000

    re-organizing app directory & ignore/prune outputs

---
+++ apps/web/src/app/components/Message.tsx
@@ -0,0 +1,19 @@
+import { MessageEntry, CharacterImages } from "./podcast";
+
+export function Message({ message }: { message: MessageEntry }) {
+  return (
+    <div className="flex items-start gap-4 text-black">
+      <div className="text-3xl flex-shrink-0 flex items-center justify-center w-20">
+        <img className="h-full" src={CharacterImages.get(message.name)} />
+      </div>
+      <div className="grid gap-1 items-start text-sm">
+        <div className="flex items-center gap-2">
+          <div className="font-bold">{message.name}</div>
+        </div>
+        <div>
+          <p>{message.message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
